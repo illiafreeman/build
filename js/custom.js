3febeee074b58839c33b630a37b961de07ad2123
freeman <iviewn@gmail.com>
2023-12-07 22:18:05 +0300

    index

--- js/custom.js
+++ js/custom.js
@@ -9,10 +9,15 @@ $('.team__gal').slick({
     variableWidth: true,
     focusOnSelect: true,
     prevArrow: `<button class="slick-prev" aria-label="Previous" type="button">
-                                <img src="./img/icon_chevron_left.svg" alt=""> 
+                                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7 1L1 7L7 13" />
+                                </svg>
+    
                             </button>`,
-                nextArrow: `<button class="slick-next" aria-label="Next" type="button">
-                                <img src="./img/icon_chevron.svg" alt="">
+    nextArrow: `<button class="slick-next" aria-label="Next" type="button">
+                                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 1L7 7L1 13" />
+                                </svg>
                             </button>`,
     responsive: [
         {
